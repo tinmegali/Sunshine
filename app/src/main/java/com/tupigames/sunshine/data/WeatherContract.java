84e1aba67369011c1f8cf9e391fbd3f3d8c77450
tinmegali <tinmegali@gmail.com>
2015-01-22 22:41:33 -0200

    Problemas no DBHelper solucionados

--- app/src/main/java/com/tupigames/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/tupigames/sunshine/data/WeatherContract.java
@@ -91,11 +91,11 @@ public class WeatherContract {
          * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698595/m-1633698597
          **/
 
-        public final String TABLE_NAME = "location";
-        public final String COLUMN_CITY_NAME        = "city_name";
-        public final String COLUMN_LOCATION_SETTING = "location_settings";
-        public final String COLUMN_COORD_LAT        = "latitude";
-        public final String COLUMN_COORD_LONG       = "longitude";
+        public static final String TABLE_NAME = "location";
+        public static final String COLUMN_CITY_NAME        = "city_name";
+        public static final String COLUMN_LOCATION_SETTING = "location_settings";
+        public static final String COLUMN_COORD_LAT        = "latitude";
+        public static final String COLUMN_COORD_LONG       = "longitude";
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
--- app/src/main/java/com/tupigames/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/tupigames/sunshine/data/WeatherDbHelper.java
@@ -20,7 +20,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.tupigames.sunshine.data.WeatherContract.LocationEntry;
 import com.tupigames.sunshine.data.WeatherContract.WeatherEntry;
-import com.tupigames.sunshine.data.WeatherContract;
 
 
 /**
