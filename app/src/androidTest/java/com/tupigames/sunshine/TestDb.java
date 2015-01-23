c41af8280c766eec2df671b0f65fa71ba14a549a
tinmegali <tinmegali@gmail.com>
2015-01-23 11:47:26 -0200

    4.09 - Teste do DB concluídos. Funcionando perfeitamente. Falta simpleficar o código.

--- app/src/androidTest/java/com/tupigames/sunshine/TestDb.java
+++ app/src/androidTest/java/com/tupigames/sunshine/TestDb.java
@@ -121,24 +121,104 @@ public class TestDb extends AndroidTestCase {
             fail("No values returned :(");
         }
 
+
+        String testDate     = "20141205";
+        double testDegrees  = 1.1;
+        double testHumidity = 1.2;
+        double testPressure = 1.3;
+        double testMax      = 75;
+        double testMin      = 60;
+        String testDesc     = "Asteroids";
+        double testWind     = 5.5;
+        double testWID      = 321;
+
         // Fantastic.  Now that we have a location, add some weather!
         ContentValues weatherValues = new ContentValues();
         weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
-        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, "20141205");
-        weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.1);
-        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.2);
-        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.3);
-        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 75);
-        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 65);
-        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
-        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 5.5);
-        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 321);
+        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, testDate);
+        weatherValues.put(WeatherEntry.COLUMN_DEGREES, testDegrees);
+        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, testHumidity);
+        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, testPressure);
+        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, testMax);
+        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, testMin);
+        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, testDesc);
+        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, testWind);
+        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, testWID);
 
         /**
          * TODO YOUR CODE BELOW HERE FOR QUIZ
          * QUIZ - 4a - InsertReadDbTest
          * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698604/m-1633698605
          **/
+
+        long weatherRowID;
+        weatherRowID =  db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
+        assertTrue(weatherRowID != -1);
+
+        String[] columnsWeather = {
+                WeatherEntry.COLUMN_DATETEXT,
+                WeatherEntry.COLUMN_DEGREES,
+                WeatherEntry.COLUMN_HUMIDITY,
+                WeatherEntry.COLUMN_LOC_KEY,
+                WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherEntry.COLUMN_MIN_TEMP,
+                WeatherEntry.COLUMN_PRESSURE,
+                WeatherEntry.COLUMN_WEATHER_ID,
+                WeatherEntry.COLUMN_WIND_SPEED,
+                WeatherEntry.COLUMN_SHORT_DESC
+        };
+
+        // A cursor is your primary interface to the query results.
+        Cursor cursorWeather = db.query(
+                WeatherEntry.TABLE_NAME,  // Table to Query
+                columnsWeather,
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        // If possible, move to the first row of the query results.
+        if (cursorWeather.moveToFirst()) {
+            // Get the value in each column by finding the appropriate column index.
+            int dateIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_DATETEXT);
+            String date = cursorWeather.getString(dateIndex);
+
+            int descIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_SHORT_DESC);
+            String description = cursorWeather.getString(descIndex);
+
+            int minIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP);
+            double min = cursorWeather.getDouble(minIndex);
+
+            int maxIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP);
+            double max = cursorWeather.getDouble(maxIndex);
+
+            int humidityIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY);
+            double humidity = cursorWeather.getDouble(humidityIndex);
+
+            int pressureIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_PRESSURE);
+            double pressure = cursorWeather.getDouble(pressureIndex);
+
+            int windspeedIndex = cursorWeather.getColumnIndex(WeatherEntry.COLUMN_WIND_SPEED);
+            double windspeed = cursorWeather.getDouble(windspeedIndex);
+
+
+            assertEquals(testDate, date);
+            assertEquals(testDesc, description);
+            assertEquals(testPressure, pressure);
+            assertEquals(testHumidity, humidity);
+            assertEquals(testWind, windspeed);
+            assertEquals(testMax, max);
+            assertEquals(testMin, min);
+
+            // Fantastic.  Now that we have a location, add some weather!
+        } else {
+            // That's weird, it works on MY machine...
+            fail("No values returned :(");
+        }
+
+        cursorWeather.close();
 
         dbHelper.close();
     }
